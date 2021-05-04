8e871dff1d463f8bd1018772ebe1d3a3a0d4b94f
VictorTamayoBriones <victortamayo509@gmail.com>
2021-05-04 17:02:43 -0500

    base de pruebas

--- src/utils/firebase.js
+++ src/utils/firebase.js
@@ -1,6 +1,39 @@
 import firebase from "firebase/app";
 
 var firebaseConfig = {
+    apiKey: "AIzaSyDxE-mas-eIj0-js18IBmb54aiNVXeekao",
+    authDomain: "pruebas-seani.firebaseapp.com",
+    databaseURL: "https://pruebas-seani-default-rtdb.firebaseio.com",
+    projectId: "pruebas-seani",
+    storageBucket: "pruebas-seani.appspot.com",
+    messagingSenderId: "943584114326",
+    appId: "1:943584114326:web:3300cad9cb3eae74cb34b7",
+    measurementId: "G-GNV6CDLLGL"
+};
+
+export default firebase.initializeApp(firebaseConfig);
+
+/*
+        B A S E - - D E - - P R U E B A S
+
+
+    apiKey: "AIzaSyDxE-mas-eIj0-js18IBmb54aiNVXeekao",
+    authDomain: "pruebas-seani.firebaseapp.com",
+    databaseURL: "https://pruebas-seani-default-rtdb.firebaseio.com",
+    projectId: "pruebas-seani",
+    storageBucket: "pruebas-seani.appspot.com",
+    messagingSenderId: "943584114326",
+    appId: "1:943584114326:web:3300cad9cb3eae74cb34b7",
+    measurementId: "G-GNV6CDLLGL"
+
+*/
+
+
+
+/*
+        B A S E - - D E - - P R O D U C C I Ó N
+    
+
     apiKey: "AIzaSyAyiLjNMCdX4vkB_kM0n8D_KE-tWhzGGg4",
     authDomain: "seani-954ac.firebaseapp.com",
     databaseURL: "https://seani-954ac.firebaseio.com",
@@ -9,6 +42,5 @@ var firebaseConfig = {
     messagingSenderId: "821462862987",
     appId: "1:821462862987:web:43310b4bbc8b84f354d88e",
     measurementId: "G-EXBFMRJ8F4"
-};
-
-export default firebase.initializeApp(firebaseConfig);
+    
+*/
