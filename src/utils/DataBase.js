fdd1f3fa4c3e478ea4b37d02f844f6cf4893f02a
Víctor Tamayo <66786892+VictorTamayoBriones@users.noreply.github.com>
2022-01-27 11:43:34 -0600

    err db

--- src/utils/DataBase.js
+++ src/utils/DataBase.js
@@ -1,5 +1,5 @@
-import firebaseApp from "./firebase";
-import * as firebase from "firebase";
+import firebase from "./firebase";
+import "firebase/firestore";
 
 const db = firebase.firestore(firebaseApp);
 
